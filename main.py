bbfb6d8644c21a90afd9a5886e883255e5d37600
BloodBlister <peterlittleboy@hotmail.co.uk>
2025-03-01 23:39:22 +0000

    Refresh Window

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
     pygame.init
     
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
     print ("Starting Asteroids!")
@@ -17,6 +20,7 @@ def main():
         
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
